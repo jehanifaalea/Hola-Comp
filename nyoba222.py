5ae9c368cc67a365694461a97eb9a031ae654f83
jehan <jehanifaalea@gmail.com>
2023-06-05 13:29:30 +0700

    nyoba lagi

--- nyoba222.py
+++ nyoba222.py
@@ -8,22 +8,47 @@ def rekomendasi_laptop():
         if tampilkan_prosesor.lower() == "ya":
             prosesor = input("Masukkan jenis prosesor yang diinginkan: ")
             laptop_rekomendasi = [laptop for laptop in data_laptop if data_laptop[laptop]['prosesor'] == prosesor]
-        else:
-            laptop_rekomendasi = list(data_laptop.keys())  
         
-    
+
         tampilkan_ram = input("Apakah Anda ingin menampilkan berdasarkan RAM? (ya/tidak): ")
-        
         if tampilkan_ram.lower() == "ya":
             ram = int(input("Masukkan ukuran RAM (GB) yang diinginkan: "))
             laptop_rekomendasi = [laptop for laptop in laptop_rekomendasi if data_laptop[laptop]['ram'] == ram]
         
+
         tampilkan_rom = input("Apakah Anda ingin menampilkan berdasarkan ROM? (ya/tidak): ")
-        
         if tampilkan_rom.lower() == "ya":
             rom = int(input("Masukkan ukuran ROM (GB) yang diinginkan: "))
             laptop_rekomendasi = [laptop for laptop in laptop_rekomendasi if data_laptop[laptop]['rom'] == rom]
         
+        else:
+            tampilkan_harga = input("Apakah Anda ingin menampilkan berdasarkan rentang harga? (ya/tidak): ")
+        
+            if tampilkan_harga.lower() == "ya":
+                min_harga = int(input("Masukkan harga minimum: "))
+                max_harga = int(input("Masukkan harga maksimum: "))
+                laptop_rekomendasi = [laptop for laptop in data_laptop if min_harga <= data_laptop[laptop]['harga'] <= max_harga]
+            else:
+                laptop_rekomendasi = list(data_laptop.keys())  
+        
+
+            tampilkan_merk = input("Apakah Anda ingin menampilkan berdasarkan merk? (ya/tidak): ")
+        
+            if tampilkan_merk.lower() == "ya":
+                merk = input("Masukkan merk yang diinginkan: ")
+                laptop_rekomendasi = [laptop for laptop in laptop_rekomendasi if data_laptop[laptop]['merk'] == merk]
+        
+        print("Laptop rekomendasi:")
+        if laptop_rekomendasi:
+            for laptop in laptop_rekomendasi:
+                print("- Merk:", data_laptop[laptop]['merk'])
+                print("- Prosesor:", data_laptop[laptop]['prosesor'])
+                print("- RAM:", data_laptop[laptop]['ram'], "GB")
+                print("- ROM:", data_laptop[laptop]['rom'], "GB")
+                print("- Harga:", data_laptop[laptop]['harga'])
+        else:
+            print("Tidak ada laptop yang ditemukan sesuai dengan pilihan Anda.")
+
         print("Laptop rekomendasi berdasarkan spesifikasi:")
         for laptop in laptop_rekomendasi:
             print("- Merk:", data_laptop[laptop]['merk'])
