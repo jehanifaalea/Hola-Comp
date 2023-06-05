190ea3162ed36d41b98a175a00453bb160a4d580
jehan <jehanifaalea@gmail.com>
2023-06-05 13:06:39 +0700

    update

--- data_laptop.py
+++ data_laptop.py
@@ -1,11 +1,6 @@
-# File: data_laptop.py
 
 data_laptop = {
     'laptop1': {'merk': 'Acer', 'prosesor': 'Intel i5', 'ram': 8, 'rom': 256, 'harga': 5000000},
-<<<<<<< HEAD
     'laptop2': {'merk': 'Asus', 'prosesor': 'AMD Ryzen 7', 'ram': 16, 'rom': 512, 'harga': 8000000},
-=======
-    'laptop2': {'merk': 'Hp', 'prosesor': 'AMD Ryzen 7', 'ram': 16, 'rom': 512, 'harga': 8000000},
->>>>>>> cd453654843da6cc762635074c877153aacbf598
     'laptop3': {'merk': 'Dell', 'prosesor': 'Intel i7', 'ram': 16, 'rom': 1024, 'harga': 10000000}
 }
