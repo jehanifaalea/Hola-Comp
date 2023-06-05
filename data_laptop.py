c230fdead4d7076a88c12df41581190423f3af6e
Jagadadrmd <127372507+Jagadadrmd@users.noreply.github.com>
2023-06-05 13:43:37 +0700

    masukin data

--- data_laptop.py
+++ data_laptop.py
@@ -1,6 +1,23 @@
+# File: data_laptop.py
 
 data_laptop = {
     'laptop1': {'merk': 'Acer', 'prosesor': 'Intel i5', 'ram': 8, 'rom': 256, 'harga': 5000000},
-    'laptop2': {'merk': 'Asus', 'prosesor': 'AMD Ryzen 7', 'ram': 16, 'rom': 512, 'harga': 8000000},
-    'laptop3': {'merk': 'Dell', 'prosesor': 'Intel i7', 'ram': 16, 'rom': 1024, 'harga': 10000000}
+    'laptop2': {'merk': 'Hp', 'prosesor': 'AMD Ryzen 7', 'ram': 16, 'rom': 512, 'harga': 8000000},
+    'laptop3': {'merk': 'Dell', 'prosesor': 'Intel i7', 'ram': 16, 'rom': 1024, 'harga': 10000000},
+    'laptop4': {'merk': 'Asus Vivobook Pro M3400QA', 'prosesor': 'AMD Ryen 5600H', 'ram': '8', 'rom': '512', 'harga': 13000000},
+    'laptop5': {'merk': 'Dell Inspiron 14', 'prosesor': 'Intel Core i7', 'ram': '12', 'rom': '512', 'harga': 8500000},
+    'laptop6': {'merk': 'Lenovo Ideapad S340', 'prosesor': 'Intel Core i5', 'ram': '8', 'rom': '1024', 'harga': 8500000},
+    'laptop7': {'merk': 'Asus TUF Gaming A15', 'prosesor': 'AMD Ryzen 7', 'ram': '8', 'rom': '512', 'harga': 12000000},
+    'laptop8': {'merk': 'Hp Omen 15', 'prosesor': 'Intel Core i7', 'ram': '12', 'rom': '1024', 'harga': 14000000},
+    'laptop9': {'merk': 'Dell Dell G5 15', 'prosesor': 'Intel Core i7', 'ram': '8', 'rom': '512', 'harga': 12000000},
+    'laptop10': {'merk': 'Dell G5 15', 'prosesor': 'Intel Core i7', 'ram': '8', 'rom': '512', 'harga': 15000000},
+    'laptop11': {'merk': 'Lenovo Legion 7', 'prosesor': 'AMD Ryzen 7', 'ram': '12', 'rom': '1024', 'harga': 18000000},
+    'laptop12': {'merk': 'Hp HP Spectre x360', 'prosesor': 'Intel Core i7', 'ram': '8', 'rom': '512', 'harga': 17000000},
+    'laptop13': {'merk': 'Lenovo Legion 7', 'prosesor': 'AMD Ryzen 7', 'ram': '12', 'rom': '1024', 'harga': 18000000},
+    'laptop14': {'merk': 'Asus TUF Gaming A15 FA507', 'prosesor': 'AMD Ryzen 7', 'ram': '8', 'rom': '512', 'harga': 18000000},
+    'laptop15': {'merk': 'Apple MacBook Pro 16', 'prosesor': 'Intel Core i7', 'ram': '16', 'rom': '512', 'harga': 30000000},
+    'laptop16': {'merk': 'HP Spectre x360 15', 'prosesor': 'Intel Core i7', 'ram': '16', 'rom': '1024', 'harga': 23000000},
+    'laptop17': {'merk': 'Acer Predator Helios 16', 'prosesor': 'Intel Core i7', 'ram': '16', 'rom': '1024', 'harga': 28000000},
+    'laptop18': {'merk': 'Lenovo IdeaPad Gaming 3', 'prosesor': 'AMD Ryzen 5', 'ram': '8', 'rom': '512', 'harga': 11000000},
+    'laptop18': {'merk': 'Dell Inspiron 15 5509', 'prosesor': 'Intel Core i7', 'ram': '16', 'rom': '512', 'harga': 13000000}
 }
